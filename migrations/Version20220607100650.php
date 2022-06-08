87cb62a39fc415759d24916259f4a30408b7be0f
steven-syntra <info@inform.be>
2022-06-08 17:43:51 +0200

    After JSON Login, before JWT

--- migrations/Version20220607100650.php
+++ migrations/Version20220607100650.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220514135022 extends AbstractMigration
+final class Version20220607100650 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,12 @@ final class Version20220514135022 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE teacher (id INT AUTO_INCREMENT NOT NULL, naam VARCHAR(255) NOT NULL, 
-voornaam VARCHAR(255) DEFAULT NULL, geboortedatum DATE DEFAULT NULL, 
-specialisatie VARCHAR(255) DEFAULT NULL, 
-PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, naam VARCHAR(255) DEFAULT NULL, voornaam VARCHAR(255) DEFAULT NULL, telefoon VARCHAR(50) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE teacher');
+        $this->addSql('DROP TABLE user');
     }
 }
--- migrations/Version20220607105059.php
+++ migrations/Version20220607105059.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220518130319 extends AbstractMigration
+final class Version20220607105059 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,12 @@ final class Version20220518130319 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE punten_detail (id INT AUTO_INCREMENT NOT NULL, student_id INT NOT NULL, datum DATE DEFAULT NULL, aantal 
-    INT NOT NULL, INDEX i_student_id (student_id), PRIMARY KEY(id)) 
-    DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE punten_detail ADD CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES student (id)');
+        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE punten_detail');
+        $this->addSql('ALTER TABLE user DROP is_verified');
     }
 }
